032d9f4da18961b87d5ffdf973d17e06dd210807
deril <bignyak@bigmir.net>
2015-02-04 00:46:49 +0200

    Add base model recipe

---
+++ db/migrate/20150203204326_create_recipes.rb
@@ -0,0 +1,13 @@
+class CreateRecipes < ActiveRecord::Migration
+  def change
+    create_table :recipes do |t|
+      t.string :title
+      t.text :description
+      t.integer :ingredients_count
+      t.integer :cooking_time
+      t.string :complexity
+
+      t.timestamps null: false
+    end
+  end
+end
