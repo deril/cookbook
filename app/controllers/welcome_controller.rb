822f91e1b9b4153bbae15d22395a392064ecfda4
deril <bignyak@bigmir.net>
2015-02-21 13:05:43 +0200

    Add ingridients for listed recipe

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     @recipe_listed = Recipe.recipe_of_the_day
+    @listed_ingredients = @recipe_listed.ingredients.pluck(:value)
+    @listed_ingredients.take(11) << '. . .' if @listed_ingredients.length > 12
   end
 end
